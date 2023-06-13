02f5fffb4fc16b73392e52ea916b9a85cf47c651
Odysseus64 <jotarokudjo01@gmail.com>
2023-06-13 17:35:55 +0600

    Commit message

---
+++ src/test/java/com/example/Auto_Star/AutoStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auto_Star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
